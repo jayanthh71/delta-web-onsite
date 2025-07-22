60f4939f0bf9e84353bfd601a1938c709b4f362f
jayanthh71 <jayanth37069@gmail.com>
2025-07-22 19:16:25 +0530

    feat: get repo commits from api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,41 @@
+"use client";
+
+import getRepoCommits from "@/lib/getRepoCommits";
+import { useState } from "react";
+
 export default function Home() {
-  return <div>Hello World</div>;
+  const [repoUrl, setRepoUrl] = useState("");
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const repo = repoUrl.trim();
+    if (repo) {
+      getRepoCommits(repo);
+    } else {
+      console.error("Please enter a valid repository URL.");
+    }
+  };
+
+  return (
+    <div className="mt-10 flex min-h-screen flex-col items-center">
+      <h1 className="font-sans text-4xl font-bold">Git log Visualizer</h1>
+      <form className="flex flex-col items-center" onSubmit={handleSubmit}>
+        <input
+          className="mt-16 w-3xl rounded border border-gray-300 p-2 text-center"
+          type="text"
+          placeholder="Enter Git repository URL"
+          value={repoUrl}
+          onChange={(e) => setRepoUrl(e.target.value)}
+          required
+          autoFocus
+        />
+        <button
+          type="submit"
+          className="mt-10 w-lg cursor-pointer rounded-lg bg-blue-600 p-2 text-white hover:bg-blue-700 active:bg-blue-800"
+        >
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 }
---
+++ src/lib/getRepoCommits.ts
@@ -0,0 +1,30 @@
+export default async function getRepoCommits(repo: string) {
+  const owner = repo.split("/")[3];
+  const repoName = repo.split("/")[4].replace(".git", "");
+
+  const apiUrl = `https://api.github.com/repos/${owner}/${repoName}/commits`;
+  const headers = {
+    Accept: "application/vnd.github+json",
+    Authorization: `Bearer ${process.env.NEXT_PUBLIC_GITHUB_TOKEN}`,
+    "X-GitHub-Api-Version": "2022-11-28",
+  };
+
+  const commitData = await fetch(apiUrl, {
+    method: "GET",
+    headers: headers,
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`HTTP error status: ${response.status}`);
+      }
+      return response.json();
+    })
+    .then((data) => {
+      return data;
+    })
+    .catch((error) => {
+      console.error("Error fetching commit data:", error);
+    });
+
+  console.log(commitData);
+}
